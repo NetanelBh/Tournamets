e9660a329afceceac11be328b7a59b536e394ad2
Netanel <netanel77777@gmail.com>
2025-06-18 13:54:03 +0300

    Add comment to Api.js file

--- client/src/components/utils/Api.js
+++ client/src/components/utils/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Axios base url define to use it when send requests to backend
 const API = axios.create({
   baseURL: "http://localhost:3000",
   withCredentials: true

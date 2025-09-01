c9177851d18b86fd7f3ed16a8706daeb95046f5d
Netanel <netanel77777@gmail.com>
2025-09-01 10:53:33 +0300

    Payment method

--- client/src/components/pages/bets/MyBets.jsx
+++ client/src/components/pages/bets/MyBets.jsx
@@ -4,7 +4,7 @@ const MyBets = () => {
     const allTournaments = useSelector((state) => state.tournaments.tournaments);
     // Get the current tournament to use the teams for the winner team prediction of the user
     const currentTourmanent = allTournaments.find((t) => t._id === localStorage.getItem("tournamentId"));
-    console.log(currentTourmanent);
+    // console.log(currentTourmanent);
     
     // TODO: CREATE A LIST WITH THE MATCHES THAT DIDN'T START YET
   return (
--- client/src/components/pages/layouts/BetsLayout.jsx
+++ client/src/components/pages/layouts/BetsLayout.jsx
@@ -3,17 +3,24 @@ import BetsHeader from "../bets/BetsHeader";
 import { useSelector } from "react-redux";
 
 const BetsLayout = () => {
-	// TODO: ADD LINK TO PAYBOX WHEN CLICK ON THE BUTTON
+	// Get the entire user's groups
+	const userGroups = useSelector((state) => state.user.user.groups);
+	// Extract only the current group
+	const currentGroup = userGroups.find((g) => g._id === localStorage.getItem("groupId"));
+
+	// TODO: CHECK HOW TO OPEN NEW TAB FOR THE PAYMENT WHEN CLICK ON THE PAYMENT BUTTON
 	const now = new Date().toISOString();
 	const tournamentId = localStorage.getItem("tournamentId");
 
 	const tournament = useSelector((state) => state.tournaments.tournaments.find((t) => t._id === tournamentId));
+	// Check if the tournament is started
 	const isTournamentStarted = now > tournament.startTime;
 	return (
 		<div className="flex flex-col items-center">
 			<BetsHeader />
 
-			{!isTournamentStarted && (
+			{/* Show only if the group defined with payment and if the tournament didn't start yet */}
+			{!isTournamentStarted && currentGroup.isPaid && (
 				<button
 					className="mb-8 mt-4 w-full sm:w-1/4 bg-[#0018F9] shadow-md shadow-gray-400/80 hover:shadow-sm hover:scale-95 active:scale-95 active:shadow-sm text-white font-bold py-2.5 rounded-lg transition-colors"
 					type="submit"

a29b256181a52c5848843b15ec744c1e58c25e65
Netanel <netanel77777@gmail.com>
2025-11-14 01:25:13 +0200

    Add commit

--- client/src/components/pages/bets/Table.jsx
+++ client/src/components/pages/bets/Table.jsx
@@ -1,8 +1,67 @@
 const Table = () => {
-    // TODO: SHOW POINTS TABLE, RELATES TO THE SCORE METHOD(IF IS DIFFERENT OR SAME IN THE CURRENT GROUP)
-  return (
-    <div>Table</div>
-  )
-}
+	// TODO: SHOW POINTS TABLE, RELATES TO THE SCORE METHOD(IF IS DIFFERENT OR SAME IN THE CURRENT GROUP)
+	return (
+		<div className="flex flex-col">
+			<div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+				<div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+					<div className="overflow-hidden">
+						<table className="min-w-full border text-center text-xs font-light text-white dark:border-neutral-500">
+							<thead className="border-b font-medium dark:border-neutral-500">
+								<tr>
+									<th scope="col" className="px-4 py-2 dark:border-neutral-500">
+										#
+									</th>
+									<th scope="col" className="px-4 py-2 dark:border-neutral-500">
+										שם
+									</th>
+									<th scope="col" className="px-4 py-2 dark:border-neutral-500">
+										Last
+									</th>
+									<th scope="col" className="px-4 py-2 dark:border-neutral-500">
+										Handle
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr className="border-b dark:border-neutral-500">
+									<td className="whitespace-nowrap  px-4 py-4 font-medium dark:border-neutral-500">
+										1
+									</td>
+									<td className="whitespace-nowrap  px-4 py-4 dark:border-neutral-500">
+										Mark
+									</td>
+									<td className="whitespace-nowrap  px-4 py-4 dark:border-neutral-500">
+										Otto
+									</td>
+									<td className=" px-4 py-4 dark:border-neutral-500">@mdo</td>
+								</tr>
+								<tr className="border-b dark:border-neutral-500">
+									<td className="whitespace-nowrap  px-4 py-4 font-medium dark:border-neutral-500">
+										2
+									</td>
+									<td className="whitespace-nowrap  px-4 py-4 dark:border-neutral-500">
+										Jacob
+									</td>
+									<td className="whitespace-nowrap  px-4 py-4 dark:border-neutral-500">
+										Thornton
+									</td>
+									<td className=" px-4 py-4 dark:border-neutral-500">@fat</td>
+								</tr>
+								<tr className="border-b dark:border-neutral-500">
+									<td className="whitespace-nowrap  px-4 py-4 font-medium dark:border-neutral-500">
+										3
+									</td>
+									<td className=" px-4 py-4 dark:border-neutral-500">Larry the Bird</td>
+									<td className=" px-4 py-4 dark:border-neutral-500">@twitter</td>
+									<td className=" px-4 py-4 dark:border-neutral-500">@twitter</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Table
+export default Table;
--- server/routes/tournamentsRoute.js
+++ server/routes/tournamentsRoute.js
@@ -24,68 +24,68 @@ router.post("/create", async (req, res) => {
 	try {
 		const { name, startDate, endDate, startTime, topScorerBet, imgUrl, topScorersList } = req.body;
 
-		// Remove the _ from the original name to store in DB(the name with _ just to fetch data from wikipedia)
-		const dbName = name.replace(/_/g, " ");
+		// // Remove the _ from the original name to store in DB(the name with _ just to fetch data from wikipedia)
+		// const dbName = name.replace(/_/g, " ");
 
-		const isTournamentExist = await tournamentServices.getTournamentByName(dbName);
-		if (isTournamentExist) {
-			res.send({ status: false, data: "הטורניר כבר קיים" });
-			return;
-		}
+		// const isTournamentExist = await tournamentServices.getTournamentByName(dbName);
+		// if (isTournamentExist) {
+		// 	res.send({ status: false, data: "הטורניר כבר קיים" });
+		// 	return;
+		// }
 
-		// Should convert the date entered to DATE object as UTC
-		const utcDate = israelToUTC(startDate, startTime);
-
+		// // Should convert the date entered to DATE object as UTC
+		// const utcDate = israelToUTC(startDate, startTime);
+		
 		// Get the fixtures from wikipedia (translated to hebrew)
 		const fixtures = await fetchFixtures(name);
-
-		// From the fixtures, extract the teams(with set to avoid duplicates)
-		const teams = new Set();
-		for (const fixture of fixtures) {
-			teams.add(fixture.home);
-			teams.add(fixture.away);
-		}
-
-		// Convert the set to array to insert the DB
-		const teamsArray = Array.from(teams);
-
-		const players = [];
-		// If the tournament includes bet on the top scorer store the players in the DB(players collection)
-		if (topScorerBet) {
-			for (const name of topScorersList) {
-				try {
-					const player = await createPlayer(name);
-					players.push(player._id);
-				} catch (error) {
-					res.send({ status: false, data: "אירעה בעיה ביצירת השחקנים, אנא נסה שנית" });
-					return;
-				}
-			}
-		}
-
-		teamsArray[8] = "TBD";
+		console.log(fixtures);
+		
 		
 
-		// Create the tournament
-		const tournament = await tournamentServices.create(
-			dbName,
-			endDate,
-			utcDate,
-			imgUrl,
-			topScorerBet,
-			teamsArray,
-			players
-		);
+		// // From the fixtures, extract the teams(with set to avoid duplicates)
+		// const teams = new Set();
+		// for (const fixture of fixtures) {
+		// 	teams.add(fixture.home);
+		// 	teams.add(fixture.away);
+		// }
+
+		// // Convert the set to array to insert the DB
+		// const teamsArray = Array.from(teams);
+
+		// const players = [];
+		// // If the tournament includes bet on the top scorer store the players in the DB(players collection)
+		// if (topScorerBet) {
+		// 	for (const name of topScorersList) {
+		// 		try {
+		// 			const player = await createPlayer(name);
+		// 			players.push(player._id);
+		// 		} catch (error) {
+		// 			res.send({ status: false, data: "אירעה בעיה ביצירת השחקנים, אנא נסה שנית" });
+		// 			return;
+		// 		}
+		// 	}
+		// }
+
+		// // Create the tournament
+		// const tournament = await tournamentServices.create(
+		// 	dbName,
+		// 	endDate,
+		// 	utcDate,
+		// 	imgUrl,
+		// 	topScorerBet,
+		// 	teamsArray,
+		// 	players
+		// );
 		
-		if (!tournament) {
-			res.send({ status: false, data: "טורניר לא נוצר, אנא נסה שנית" });
-			return;
-		}
+		// if (!tournament) {
+		// 	res.send({ status: false, data: "טורניר לא נוצר, אנא נסה שנית" });
+		// 	return;
+		// }
 
-		// Create a job scheduler to find unpaid users in group that included payment and delete them from the group
-		findUnpaidUsers(tournament._id);
+		// // Create a job scheduler to find unpaid users in group that included payment and delete them from the group
+		// findUnpaidUsers(tournament._id);
 
-		res.send({ status: true, data: tournament });
+		// res.send({ status: true, data: tournament });
 	} catch (error) {
 		console.log(error);
 		

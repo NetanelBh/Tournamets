4b692d61767d1f4355e58927b2dd59e223888c41
Netanel <netanel77777@gmail.com>
2025-11-14 01:25:11 +0200

    Add header to axios when send request to google

--- server/utils/fixturesFromWiki.js
+++ server/utils/fixturesFromWiki.js
@@ -5,9 +5,15 @@ import teamNameTranslation from "./teamNameTranslation.js";
 const fixturesFetch = async (name) => {
 	const url = `https://en.wikipedia.org/w/api.php?action=parse&page=${name}&prop=text&formatversion=2&format=json`;
 	try {
-		const res = await axios.get(url);
+		const res = await axios.get(url, {
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+				"Accept-Language": "en-US,en;q=0.9",
+			},
+		});
 		const data = res.data;
-
+		
 		const $ = cheerio.load(data.parse.text);
 
 		// Each match box is wrapped in .footballbox
@@ -19,7 +25,7 @@ const fixturesFetch = async (name) => {
 				date: $(el).find(".fdate").text().trim(),
 				score: $(el).find(".fscore").text().trim(),
 			});
-		});
+		});		
 
 		// Create a new object to overlaod the mutable fixtures got from wikipedia
 		const newFixtures = [];

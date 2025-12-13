6258c8dcc029e41d5bcf7c498b90bc28997f4a4d
Netanel <netanel77777@gmail.com>
2025-12-14 00:19:06 +0200

    Fix bugs

--- client/src/components/store/store.js
+++ client/src/components/store/store.js
@@ -1,8 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 // defaults to localStorage
 import storage from "redux-persist/lib/storage";
-import { combineReducers } from "redux";
 
 import betsReducer from "./slices/betSlice.js";
 import userReducer from "./slices/userSlice.js";

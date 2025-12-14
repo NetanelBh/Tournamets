dac799c3e20a967b4403cfb3a2e6d9cd6ec27cad
Netanel <netanel77777@gmail.com>
2025-12-14 02:29:36 +0200

    Fix bug

--- client/src/components/pages/register/Register.jsx
+++ client/src/components/pages/register/Register.jsx
@@ -87,7 +87,7 @@ const Register = () => {
 								<input
 									type="text"
 									className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
-									placeholder="יוצג בטבלאות הניקוד"
+									placeholder="יוצג בטבלת הניקוד"
 									id="username"
 									autoComplete="username"
 									ref={usernameRef}

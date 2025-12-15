756bd858772da410fa73789e55b047a443b02477
Netanel <netanel77777@gmail.com>
2025-12-16 00:15:59 +0200

    Bug fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -74,7 +74,7 @@ router.post("/register", async (req, res) => {
 
 		const token = jwt.sign({ id: createdUser._id }, process.env.JWT_SECRET, { expiresIn: "5m" });
 		const verificationLink = `${process.env.REACT_ADDRESS}/verify/${token}`;
-		sendEmail(
+		await sendEmail(
 			createdUser.email,
 			"אימות חשבון",
 			`

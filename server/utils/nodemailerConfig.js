28746b0933858ec26388fe9f4b8f2a7a5eb1a5b3
Netanel <netanel77777@gmail.com>
2025-12-17 00:30:28 +0200

    Fix bugs

--- server/utils/nodemailerConfig.js
+++ server/utils/nodemailerConfig.js
@@ -1,20 +1,6 @@
 // import nodemailer from "nodemailer";
 import sgmail from "@sendgrid/mail";
 
-// const transporter = nodemailer.createTransport({
-// 	host: "smtp.gmail.com",
-// 	port: 465,
-// 	secure: true,
-// 	authMethod: "PLAIN",
-// 	auth: {
-// 		user: process.env.SENDGRID_EMAIL,
-// 		pass: process.env.GMAIL_APP_PASSWORD,
-// 	},
-// 	connectionTimeout: 10000, // 10 seconds
-// 	greetingTimeout: 10000,
-// 	socketTimeout: 10000,
-// });
-
 sgmail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async(to, subject, text) => {

3372e10f42cedfad5cb1556d686fe35abe8f9aad
Netanel <netanel77777@gmail.com>
2026-01-12 00:12:18 +0200

    Size fix - DONE

--- client/src/components/pages/bets/MyBets.jsx
+++ client/src/components/pages/bets/MyBets.jsx
@@ -458,7 +458,7 @@ const MyBets = () => {
 					{!openModal && (
 						<div className="flex flex-col mb-6">
 							<div className="flex gap-8">
-								<div className="flex flex-col gap-2 p-4 border border-yellow-100 w-45 sm:w-50 md:w-60">
+								<div className="flex flex-col gap-2 p-4 border border-yellow-100 w-40 sm:w-50 md:w-60">
 									<h3 className="text-md text-yellow-100 text-center">האלופה :</h3>
 									{/* Show the dropdown option only if the tournament didn't start */}
 									{!istournamentStarted && (
@@ -487,7 +487,7 @@ const MyBets = () => {
 
 								{/* Show the topScorer dropdown only if the tournament defined to be with top scorer bet */}
 								{currentTourmanent.topScorerBet && (
-									<div className="flex flex-col gap-2 p-4 border border-yellow-100 w-45 sm:w-50 md:w-60">
+									<div className="flex flex-col gap-2 p-4 border border-yellow-100 w-40 sm:w-50 md:w-60">
 										<h3 className="text-md text-yellow-100 text-center">מלך השערים :</h3>
 										{!istournamentStarted && (
 											<>

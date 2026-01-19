224b990a8e55f47fa2b39f3a2f7e2a73b575859e
Netanel <netanel77777@gmail.com>
2026-01-19 21:14:33 +0200

    Fix message

--- server/index.js
+++ server/index.js
@@ -50,6 +50,7 @@ const reScheduleOnRestartServer = async (retries = 3) => {
 	try {
 		const notStartedTournaments = await getScheduledTournaments();
 		await Promise.all(
+			// Use map function because forEach doens't wait for async fuctions
 			notStartedTournaments.map((t) => findUnpaidUsers(t._id))
 		);
 	} catch (error) {
